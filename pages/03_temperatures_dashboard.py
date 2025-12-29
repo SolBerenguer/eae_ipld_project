35857cf520e88d9b690c9644b43ad7d6530e04a4
Solberenguer <solcibr@hotmail.com>
2025-12-29 20:02:15 +0100

    <updates>

--- pages/03_temperatures_dashboard.py
+++ pages/03_temperatures_dashboard.py
@@ -24,7 +24,7 @@ st.divider()
 def load_data():
     data_path = "data/cities_temperatures.csv"
 
-    temps_df = None  # TODO: Ex 3.1: Load the dataset using Pandas, use the data_path variable and set the index column to "show_id"
+    temps_df = pd.read_csv(data_path)  # TODO: Ex 3.1: Load the dataset using Pandas, use the data_path variable and set the index column to "show_id"
 
     if temps_df is not None:
         temps_df["Date"] = pd.to_datetime(temps_df["Date"]).dt.date
@@ -43,26 +43,28 @@ with st.expander("Check the complete dataset:"):
 
 # TODO: Ex 3.2: Create a new column called `AvgTemperatureCelsius` that contains the temperature in Celsius degrees.
 # temps_df["AvgTemperatureCelsius"] = ...       # uncomment this line to complete it
+temps_df["AvgTemperatureCelsius"] = (temps_df["AvgTemperatureFahrenheit"] - 32) * 5 / 9
 
 
 # ----- Extracting some basic information from the dataset -----
 
 # TODO: Ex 3.3: How many different cities are there? Provide a list of them.
-unique_countries_list = None
+unique_countries_list = temps_df["City"].unique().tolist()
 
 # TODO: Ex 3.4: Which are the minimum and maximum dates?
-min_date = None
-max_date = None
+min_date = temps_df["Date"].min()
+max_date = temps_df["Date"].max()
 
 # TODO:  Ex 3.5: What are the global minimum and maximum temperatures? Find the city and the date of each of them.
-min_temp = None
-max_temp = None
+min_temp = temps_df["AvgTemperatureCelsius"].min()
+max_temp = temps_df["AvgTemperatureCelsius"].max()
 
-min_temp_city = None
-min_temp_date = None
 
-max_temp_city = None
-max_temp_date = None
+min_temp_city = min_row["City"]
+min_temp_date = min_row["Date"]
+
+max_temp_city = max_row["City"]
+max_temp_date = max_row["Date"]
 
 
 # ----- Displaying the extracted information metrics -----
@@ -119,18 +121,34 @@ if unique_countries_list is not None and len(selected_cities) > 0:
 
     c = st.container(border=True)
 
+    selected_cities = ["Munich", "Buenos Aires", "Tokyo"]
+    start_date = pd.to_datetime("2008-01-01").date()
+    end_date = pd.to_datetime("2010-12-31").date()
+
+
     # TODO: Ex 3.7: Plot the temperatures over time for the selected cities for the selected time period,
     # every city has to be its own line with a different color.
 
+
+
     fig = plt.figure(figsize=(10, 5))
 
-    # for city in selected_cities:
-    #     city_df = None            # TODO
-    #     city_df_period = None     # TODO
-    #     plt.plot()                # TODO 
-    # plt.title()   # TODO
-    # plt.xlabel()  # TODO
-    # plt.ylabel()  # TODO
+    for city in selected_cities:
+        city_df = temps_df[temps_df["City"] == city]            # TODO
+       city_df_period = city_df[
+        (city_df["Date"] >= start_date) &
+        (city_df["Date"] <= end_date)
+    ]     
+    
+    
+         plt.plot(
+             city_df_period["Date"],
+        city_df_period["AvgTemperatureCelsius"],
+        label=city,
+        marker="o")                 
+     plt.title(f"Temperature Comparison ({start_date} to {end_date})")   # TODO
+     plt.xlabel("Date")  # TODO
+     plt.ylabel("Temperature (°C)")  # TODO
 
     plt.legend()
     
@@ -143,14 +161,20 @@ if unique_countries_list is not None and len(selected_cities) > 0:
 
     fig = plt.figure(figsize=(10, 5))
 
-    # for city in selected_cities:
-    #     city_df = None            # TODO
-    #     city_df_period = None     # TODO
-    #     plt.hist()                # TODO
+     for city in selected_cities:
+         city_df = temps_df[temps_df["City"] == city]            # TODO
+         city_df_period = city_df[
+        (city_df["Date"] >= start_date) &
+        (city_df["Date"] <= end_date)
+    ]     
+         plt.hist(city_df_period["AvgTemperatureCelsius"],
+        bins=20,
+        label=city,
+        alpha=0.6)                # TODO
 
-    # plt.title()   # TODO
-    # plt.xlabel()  # TODO
-    # plt.ylabel()  # TODO
+     plt.title(f"Temperature Distribution Comparison ({start_date} to {end_date})")   # TODO
+     plt.xlabel("Temperature (°C)")  # TODO
+     plt.ylabel("Frequency")  # TODO
 
     plt.legend()
 
